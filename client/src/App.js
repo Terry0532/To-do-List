f075322a65873bd887630fb577cb6a69b724a1cf
Terry <t3rrygong@gmail.com>
2021-04-29 20:58:56 -0600

    print out all todos when open page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,50 @@
 import React from "react";
 import API from "../src/api";
+import "./App.css";
 
 class App extends React.Component {
+  state = {
+    todolist: [],
+    todo: ""
+  }
+
   componentDidMount() {
+    //get all todos and print it
     API.allTodos().then(res => {
-      console.log(res);
+      this.setState({ todolist: res.data });
+      console.log(this.state.todolist)
     }).catch(err => {
       console.log(err);
     })
   }
 
+  handleInputChange = e => {
+    //save user input
+    this.setState({ todo: e.target.value });
+  }
+
+  addTodo = () => {
+    console.log(this.state.todo)
+
+    //clear user input after click add button
+    this.setState({ todo: "" });
+  }
+
   render() {
     return (
       <div>
-        <textarea></textarea>
-        <button>submit</button>
+        <textarea onChange={this.handleInputChange} value={this.state.todo}></textarea>
+        <button onClick={this.addTodo}>add</button>
+        <p>List:</p>
+        <table>
+          <tbody>
+            {this.state.todolist.map(todo => (
+              <tr>
+                <td>{todo.content}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     )
   }
--- routes/api/add.js
+++ routes/api/add.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 router.post("/", (req, res) => {
     console.log(req.body)
     db.todos.create({
-        progress: req.body.progress,
+        progress: 0,
         content: req.body.content
     }).then(data => res.status(200).json(data)).catch(err => res.status(500).json(err));
 });
